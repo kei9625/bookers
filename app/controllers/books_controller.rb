cb8c599e626bed19a7e9219468a80f8d93349b36
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-05-14 23:12:10 +0000

    [wip]index画面のレイアウト、詳細画面のリンクの作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,29 @@
 class BooksController < ApplicationController
-  def books
+  #コントローラの中にあるものを『アクション』、その中のものを『メソッド』
+  def new
+    @book = Book.new
   end
-  
-  def edit
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def edit
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
 
+  private
+  #ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top' #topページ
+  get '/books' => 'books#index'
+  post '/books' => 'books#create'
+  get '/books:id' => 'books#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
