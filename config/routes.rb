6ca17fbe8bb6cd979ac57f4418b25050c36ac39c
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-05-16 04:10:28 +0000

    ルーティングにresources　を実装。個別で書いていたルートは削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root to: 'homes#top' #topページ
-  get '/books' => 'books#index'
-  post '/books' => 'books#create'
-  get '/books/:id' => 'books#show', as: 'book'
-  get '/books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch '/books/:id' => 'books#update', as: 'update_book'
-  delete '/books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books
+  #resourcesで下記は最適な物を自動生成されている。必要に応じてルートパスが変更されたら、最適化されたものに書き直す
+  #get '/books' => 'books#index'
+  #post '/books' => 'books#create'
+  #get '/books/:id' => 'books#show', as: 'book'
+  #get '/books/:id/edit' => 'books#edit', as: 'edit_book'
+  #patch '/books/:id' => 'books#update', as: 'update_book'
+  #delete '/books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
