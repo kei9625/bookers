02fcec5fec46572975e09b82f45cf319450b115f
KeiMonden <129693543+kei9625@users.noreply.github.com>
2023-05-16 06:17:24 +0000

    バリデーションの追加完了。フラッシュメッセージを追加する。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,18 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
-    @books = Book.all
     @book = Book.new
+    @books = Book.all
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top' #topページ
   resources :books
-  #resourcesで下記は最適な物を自動生成されている。必要に応じてルートパスが変更されたら、最適化されたものに書き直す
+  #resourcesで下記は最適な物を自動生成されている。必要に応じてルートパスが変更されたら、最適化されたものに書き直す。
   #get '/books' => 'books#index'
   #post '/books' => 'books#create'
   #get '/books/:id' => 'books#show', as: 'book'
